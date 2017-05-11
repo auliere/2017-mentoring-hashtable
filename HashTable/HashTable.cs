ad4276d3ad7578d7c5ea98e118cee014524fab35
Oleh Pedorenko <olezhkoo@gmail.com>
2017-05-11 17:31:23 +0300

    Added tests.

---
+++ HashTable/HashTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableImpl
+{
+    public class HashTable : IHashTable
+    {
+        public object this[object key]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(object key, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGet(object key, out object value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HashTableTests/HashTableTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HashTableImpl;
+
+namespace HashTableTests
+{
+    [TestClass]
+    public class HashTableTests
+    {
+        [TestMethod]
+        public void HashTableContainsTest()
+        {
+            var ht = new HashTable();
+            string key = "KEY";
+
+
+        }
+    }
+}
